2a7be04759f2b22782012a6004ec806f06e51970
donghyun.suh <adamsuh008@gmail.com>
2025-10-26 13:51:25 +0900

    등락률 상위 30종목 API와 응답 데이터 모델 추가

--- src/api/ranking.py
+++ src/api/ranking.py
@@ -1,11 +1,37 @@
 from src.http import api_get
-from src.model.ranking import HtsTopViewBody
+from src.model.ranking import HtsTopViewBody, FluctuationBody
 
-# --- 3) HTS 조회 상위 20종목 ---
+# --- HTS 조회 상위 20종목 ---
 def get_hts_top_view() -> HtsTopViewBody:
-    path = "uapi/domestic-stock/v1/ranking/hts-top-view"
+    path = 'uapi/domestic-stock/v1/ranking/hts-top-view'
     resp = api_get(
         path=path,
         tr_id='HHMCM000100C0'
     )
     return HtsTopViewBody.model_validate(resp.json())
+
+# --- 등락률 상위 30종목 ---
+def get_fluctuation() -> FluctuationBody:
+    path = 'uapi/domestic-stock/v1/ranking/fluctuation'
+    params = {
+        'fid_cond_mrkt_div_code': 'J',
+        'fid_cond_scr_div_code': '20170',
+        'fid_input_iscd': '0000',
+        'fid_rank_sort_cls_code': '0',
+        'fid_input_cnt_1': '0',
+        'fid_prc_cls_code': '1',
+        'fid_input_price_1': '',
+        'fid_input_price_2': '',
+        'fid_vol_cnt': '',
+        'fid_rsfl_rate1': '',
+        'fid_rsfl_rate2': '',
+        'fid_trgt_cls_code': '0',
+        'fid_trgt_exls_cls_code': '0',
+        'fid_div_cls_code': '0'
+    }
+    resp = api_get(
+        path=path,
+        params=params,
+        tr_id='FHPST01700000'
+    )
+    return FluctuationBody.model_validate(resp.json())
